3629a8d31ce05b46ab82bc5f4d8a12d938245bdb
ASTR0N0M3R11 <67753327+ASTR0N0M3R11@users.noreply.github.com>
2021-07-27 09:57:52 -0400

    Cylinder Module

---
+++ Cylinder/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cylinder cylinder = new Cylinder(5.55, 7.25);
+
+        System.out.println(cylinder.getRadius());
+        System.out.println(cylinder.getHeight());
+        System.out.println(cylinder.getArea());
+        System.out.println(cylinder.getVolume());
+    }
+}
