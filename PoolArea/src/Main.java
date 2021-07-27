550ada613651df826dae5b4aa454bedc59c0b53f
ASTR0N0M3R11 <67753327+ASTR0N0M3R11@users.noreply.github.com>
2021-07-27 11:07:31 -0400

    PoolArea Module

---
+++ PoolArea/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Cuboid cuboid = new Cuboid(5, 10, 5);
+
+        System.out.println(cuboid.getWidth());
+        System.out.println(cuboid.getLength());
+        System.out.println(cuboid.getArea());
+        System.out.println(cuboid.getHeight());
+        System.out.println(cuboid.getVolume());
+    }
+}
